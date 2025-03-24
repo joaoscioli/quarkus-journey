10d6721943c2aabc4a67f37dc7603539df2348c1
joaoscioli <joaoscioli@outlook.com>
2025-03-24 20:42:03 -0300

    feat(new project) add files project junior task-manager-api

---
+++ quarkus in action/quarkus_projects_test/quarkus_junior_projects/task-manager-api/src/main/java/entity/Task.java
@@ -0,0 +1,11 @@
+package entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Task extends PanacheEntity {
+    public String title;
+    public String description;
+    public boolean completed;
+}
